30438da2ef28d0d05e5bb203bc37bbf020201e07
Fiama Arrua <fiamaarrua5@gmail.com>
2024-08-11 11:50:02 -0300

    Generador de email

---
+++ Generador de emial.py
@@ -0,0 +1,9 @@
+nombre = input("Cual es tu nombre?")
+apellido = input ("Cual es tu apellido?")
+
+gmail= "@gmail.com"
+email = nombre.lower() + "." + apellido.lower() + gmail
+
+print("Tu nuevo email generado por el sistema es: ")
+print(email)
+print("***Felicitaciones***")
